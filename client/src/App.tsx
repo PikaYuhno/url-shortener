4e5aba9503bfcc970a607196d6c0a47a2304077c
PikaYuhno <muazahmed019@gmail.com>
2020-09-21 21:57:53 +0200

    removed test comment

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,7 +4,6 @@ import TextField from "@material-ui/core/TextField";
 import Grid from "@material-ui/core/Grid";
 import { Alert, AlertTitle } from "@material-ui/lab";
 import "./App.css";
-//Test comment
 
 function App() {
     const [url, setUrl] = useState("");
